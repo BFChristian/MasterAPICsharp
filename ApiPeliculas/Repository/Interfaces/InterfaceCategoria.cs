6fda23b55788452e07e03c105aa565e904696b7b
bchri <bchri@LAPTOP>
2023-10-21 15:35:47 -0300

    Agregar archivos de proyecto.

---
+++ ApiPeliculas/Repository/Interfaces/InterfaceCategoria.cs
@@ -0,0 +1,16 @@
+﻿using ApiPeliculas.Models;
+
+namespace ApiPeliculas.Repository.Interfaces
+{
+    public interface InterfaceCategoria
+    {
+        ICollection<Categoria> GetCategorias();
+        Categoria GetCategoria(int categoriaId);
+        bool existeCategoria(string nombre);
+        bool existeCategoria(int id);
+        bool CrearCategoria(Categoria categoria);
+        bool ActualizarCategoria(Categoria categoria);
+        bool BorrarCategoria(Categoria categoria);
+        bool Guardar();
+    }
+}
